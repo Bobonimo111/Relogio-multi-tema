fdac223adce51b8b4a230a49f6fb5fc82bb2a248
William Rodrigues <carlosmaycon443@gmail.com>
2023-02-20 20:53:23 -0300

    Codigo melhorado

--- src/javascript/hora.js
+++ src/javascript/hora.js
@@ -3,15 +3,20 @@ function LoadElements() {
     const hora = document.getElementById("hora")
     console.log(datahora())
 
-    setInterval(function() { timer(datahora()[1], hora) }, 1000)
-    setInterval(function() { timer(datahora()[2], hora) }, 100000)
+    setInterval(function() { timer(datahora()[0], hora) }, 1000)
+    setInterval(function() { timer(datahora()[1], hora) }, 100000)
 }
 
 var datahora = function() {
-    var date = new Date()
-    var dateList = (date.toLocaleString().split(" "))
-    dateList[0] = dateList[0].replaceAll("/", "-")
-    return dateList
+    var data = new Date();  
+    
+    var hora = `${data.getHours()}` 
+    var min = `${data.getMinutes()}`.padStart(2 , 0)
+    var sec = `${data.getSeconds()}`.padStart(2, 0)
+    var strHora = `${hora} : ${min} : ${sec}`
+    var diaMesAno = `${data.getDate()} : ${data.getMonth()+1} : ${data.getFullYear()}`
+
+    return [strHora,diaMesAno]
 }
 
 var timer = function(value, obj) {
